94ad726d474f2c7f0c5aea275ad2a6840abea3ca
nrogiedc <43356043+njrogie@users.noreply.github.com>
2023-01-15 13:49:39 -0600

    Added database quick-start script for testing

--- source/databaseconnection.go
+++ source/databaseconnection.go
@@ -8,5 +8,17 @@ import (
 
 func initConnection() bool {
     fmt.Println("initializing connection.")
+    
     return true
+}
+
+func sendToDatabase(jsonBytes []byte) {
+    // Pack into known type
+    var tInfo TankInfo
+    err := json.Unmarshal(jsonBytes, tInfo)
+    if err != nil {
+        fmt.Println("Error parsing json from POST request.")
+    }
+    // Export the datapoint to the database
+
 }
---
+++ test/simulator/init-dbcontainer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#remove all previous traces of setup
+docker stop $(docker ps -q)
+docker rm $(docker ps -aq)
+docker volume rm roach
+docker image rm roach
+docker network rm mynet
+
+docker volume create roach
+
+docker network create -d bridge mynet
+
+docker run -d --name roach --hostname db --network mynet -p 26257:26257 -p 8080:8080 \
+    -v  roach:/cockroach/cockroach-data cockroachdb/cockroach:latest-v20.1 start-single-node \
+    --insecure
+
+docker exec roach ./cockroach sql --insecure --execute "CREATE DATABASE mydb;"
+docker exec roach ./cockroach sql --insecure --execute "CREATE USER me;"
+docker exec roach ./cockroach sql --insecure --execute "GRANT ALL ON DATABASE mydb TO me"
+
