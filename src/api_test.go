835b243ce56ca06d9abea6bd867e8c333efb2a96
Nick R <njrogie@gmail.com>
2023-12-29 04:30:44 +0000

    New revision of the API

---
+++ src/api_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+func TestManualOn(t *testing.T) {
+	t.Fatalf(`Test not implemented!`)
+}
+
+func TestManualOff(t *testing.T) {
+	t.Fatalf(`Test not implemented!`)
+}
+
+func TestFullRoutine(t *testing.T) {
+	t.Fatalf(`Test not implemented!`)
+}
+
+func TestPulse(t *testing.T) {
+	t.Fatalf(`Test not implemented!`)
+}
